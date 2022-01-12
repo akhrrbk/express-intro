01c80f2fa81179b3ed12bca5d9eadc704d7cde0d
Akhrorbek Abdukhamitov <akhrrbk@gmail.com>
2022-01-12 19:56:24 +0500

    forgot to add port

--- index2.js
+++ index2.js
@@ -90,7 +90,7 @@ app.post('/api/persons/', (req, res) =>{
 })
 
 // port
-const PORT = 3003
+const PORT = process.env.PORT || 3001
 app.listen(PORT, ()=>{
     console.log(`server running on port ${PORT}`);
 })
